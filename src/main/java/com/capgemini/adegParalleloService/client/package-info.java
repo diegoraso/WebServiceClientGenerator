2c316c650c0f85f30aaca68c660cde42fe349623
diegoraso <diego.raso@capgemini.com>
2017-01-10 10:02:33 +0100

    cambiato generatore client

--- src/main/java/com/capgemini/adegParalleloService/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.accenture.com/ADEG0/WSADEG1/")
-package com.capgemini.adegParalleloService.client;
--- src/main/java/com/capgemini/main/Main.java
+++ src/main/java/com/capgemini/main/Main.java
@@ -2,15 +2,8 @@ package com.capgemini.main;
 
 import java.io.File;
 import java.net.URL;
-import java.util.List;
 
 import javax.xml.namespace.QName;
-import javax.xml.ws.BindingProvider;
-import javax.xml.ws.handler.Handler;
-
-import com.capgemini.adegParalleloService.client.BLZService;
-import com.capgemini.adegParalleloService.client.BLZServicePortType;
-import com.capgemini.soap.handler.MyServiceLogHandler;
 
 public class Main  {
 
@@ -19,17 +12,17 @@ public class Main  {
 		File f= new File("src/main/resources/wsdl/BLZService.wsdl");
 		URL url = new URL(f.toURI().toString());
 		QName serviceQname =  new QName("http://www.webserviceX.NET", "GlobalWeather");
-		BLZService b = new BLZService();
-		BLZServicePortType port = b.getBLZServiceSOAP11PortHttp();
-	
-		
-		BindingProvider bindProv = (BindingProvider) port;
-		@SuppressWarnings("rawtypes")
-		List<Handler> handlers = bindProv.getBinding().getHandlerChain();
-		handlers.add(new MyServiceLogHandler());
-		bindProv.getBinding().setHandlerChain(handlers);
-		
-		port.getBank("ss");
+//		BLZService b = new BLZService();
+//		BLZServicePortType port = b.getBLZServiceSOAP11PortHttp();
+//	
+//		
+//		BindingProvider bindProv = (BindingProvider) port;
+//		@SuppressWarnings("rawtypes")
+//		List<Handler> handlers = bindProv.getBinding().getHandlerChain();
+//		handlers.add(new MyServiceLogHandler());
+//		bindProv.getBinding().setHandlerChain(handlers);
+//		
+//		port.getBank("ss");
 	//	g.getWeather("Levanto", "Italy");
 	}
 
