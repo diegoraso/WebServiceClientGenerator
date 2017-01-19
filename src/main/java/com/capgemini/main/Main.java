d0b9bbb9f84514cee393031c40c31e0da672a53e
diegoraso <diego.raso@capgemini.com>
2017-01-19 20:41:30 +0100

    gestione eccezioni

--- src/main/java/com/capgemini/main/Main.java
+++ src/main/java/com/capgemini/main/Main.java
@@ -1,16 +1,25 @@
 package com.capgemini.main;
 
+import javax.xml.ws.WebServiceFeature;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.capgemini.webservice.client.GlobalWeather;
 import com.capgemini.ws.soap.init.SoapWSInitializer;
 
 public class Main {
 
+	// Logger
+	private static final Logger log = LoggerFactory.getLogger(Main.class);
+
 	public static void main(String[] args) throws Exception {
 		System.setProperty("http.proxyHost", "10.68.64.37");
 		System.setProperty("http.proxyPort", "8081");
 
-		SoapWSInitializer.callWebServiceMethodWithSystemLogging(GlobalWeather.class, "getWeather", "","");
-		
+		Object response = SoapWSInitializer.callWebServiceMethodWithSystemLogging(GlobalWeather.class, "getWeather","","");
+		SoapWSInitializer.getWebServiceMethod(GlobalWeather.class, "getGlobalWeatherSoap");
+		log.debug("Respose {}",response);
 
 	}
 }
--- src/main/java/com/capgemini/ws/soap/init/SoapWSInitializer.java
+++ src/main/java/com/capgemini/ws/soap/init/SoapWSInitializer.java
@@ -5,23 +5,55 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.xml.ws.Service;
 import javax.xml.ws.WebEndpoint;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.capgemini.ws.soap.log.SoapRequestResponseLogger;
 
 public class SoapWSInitializer {
+	/*
+	 * Singleton Pattern 
+	 */
+	private static SoapWSInitializer instance;
 
-	// Logger
+	/*
+	 * Message log of catched exception
+	 */
+	private static final String NO_SUCH_METHOD_EXCEPTION_MSG = "Method {}({}) of specified class {} not found.  \n Cause:";
+	private static final String SECURITY_EXCEPTION_MSG = "SecurityException: Could not call method {}({}) of specified class {}. \n Cause:";
+	private static final String INVOCATION_TARGET_EXCEPTION_MSG = "InvocationTargetException: Could not call method {}({}) of specified class {}. \n Cause:";
+	private static final String ILLEGAL_ACCESS_EXCEPTION_MSG = "Cannot Access to method {}({}) of specified class {} . \n Cause:";
+	private static final String ILLEGAL_ARGUMENT_EXCEPTION_MSG = "Wrong argument when calling method {}({}) of specified class {}. \n Cause:";
+	private static final String INSTANTIATION_EXCEPTION_MSG = "Could not instantiate object of specified class {} with constructor method {}({}). \n Cause:";
+
+	/*
+	 * Logger
+	 */
 	private static final Logger log = LoggerFactory.getLogger(SoapWSInitializer.class);
 
-	// TODO fix log
-	// TODO fix handle exception
+	// TODO java doc
+
+	private SoapWSInitializer() {
+
+		super();
+	}
+
+	public static SoapWSInitializer getInstance() {
+
+		if (instance == null) {
+			instance = new SoapWSInitializer();
+		}
+
+		return instance;
+	}
+
 	/**
 	 * 
 	 * @param classz
@@ -32,10 +64,15 @@ public class SoapWSInitializer {
 
 		Object newInstance = null;
 
+		Class<?>[] a = getParametersClass(objects);
+
+		String param = getString(a);
+
+		String methodName = classz.getSimpleName();
+
 		try {
 			if (objects.length > 0) {
 
-				Class<?>[] a = getParametersClass(objects);
 				newInstance = classz.getDeclaredConstructor(a).newInstance(objects);
 
 			} else {
@@ -44,33 +81,17 @@ public class SoapWSInitializer {
 			}
 
 		} catch (InstantiationException e) {
-
-			log.error("Could not instatiate object of specified class {0} cause {1}", classz, e);
-
+			log.error(INSTANTIATION_EXCEPTION_MSG, classz, methodName, param, e);
 		} catch (IllegalAccessException e) {
-
-			log.error("Could not call constructor of specified class {0} with specified param {1} cause {2}", classz,
-					objects, e);
-
+			log.error(ILLEGAL_ACCESS_EXCEPTION_MSG, methodName, param, classz, e);
 		} catch (IllegalArgumentException e) {
-
-			log.error("Could not call constructor of specified class {0} with specified param {1} cause {2}", classz,
-					objects, e);
-
+			log.error(ILLEGAL_ARGUMENT_EXCEPTION_MSG, methodName, param, classz, e);
 		} catch (InvocationTargetException e) {
-
-			log.error("Could not call constructor of specified class {0} with specified param {1} cause {2}", classz,
-					objects, e);
-
+			log.error(INVOCATION_TARGET_EXCEPTION_MSG, methodName, param, classz, e);
 		} catch (NoSuchMethodException e) {
-
-			log.error("Could not call constructor of specified class {0} with specified param {1} cause {2}", classz,
-					objects, e);
-
+			log.error(NO_SUCH_METHOD_EXCEPTION_MSG, methodName, param, classz, e);
 		} catch (SecurityException e) {
-
-			log.error("Could not call constructor of specified class {0} with specified param {1} cause {2}", classz,
-					objects, e);
+			log.error(SECURITY_EXCEPTION_MSG, methodName, param, classz, e);
 		}
 
 		return newInstance;
@@ -86,12 +107,17 @@ public class SoapWSInitializer {
 	public static Method getWebServiceMethod(Class<? extends Service> classz, String methodName, Class<?>... objects) {
 
 		Method method = null;
+
+		String param = getString(objects);
+
 		try {
 			method = classz.getMethod(methodName, objects);
-		} catch (NoSuchMethodException e) {
-			log.error("Could not call method {0} of specified class {1}  cause {2}", methodName, classz, e);
+		} catch (IllegalArgumentException e) {
+			log.error(ILLEGAL_ARGUMENT_EXCEPTION_MSG, methodName, param, classz, e);
 		} catch (SecurityException e) {
-			log.error("Could not call method {0} of specified class {1}  cause {2}", methodName, classz, e);
+			log.error(SECURITY_EXCEPTION_MSG, methodName, param, classz, e);
+		} catch (NoSuchMethodException e) {
+			log.error(NO_SUCH_METHOD_EXCEPTION_MSG, methodName, param, classz, e);
 		}
 
 		return method;
@@ -107,27 +133,52 @@ public class SoapWSInitializer {
 	public static Object invokeWebMethod(Method method, Object instance, Object... objects) {
 
 		Object o = null;
+		Class<? extends Object> classz = instance.getClass();
+		Class<?>[] a = getParametersClass(objects);
+		String param = getString(a);
+
 		try {
 			o = method.invoke(instance, objects);
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			log.error("Could not call method {0} of specified class {1}  cause {2}", method, instance.getClass(), e);
+		} catch (IllegalAccessException e) {
+			log.error(ILLEGAL_ACCESS_EXCEPTION_MSG, method.getName(), param, classz, e);
+		} catch (IllegalArgumentException e) {
+			log.error(ILLEGAL_ARGUMENT_EXCEPTION_MSG, method.getName(), param, classz, e);
+		} catch (InvocationTargetException e) {
+			log.error(INVOCATION_TARGET_EXCEPTION_MSG, method.getName(), param, classz, e);
+		} catch (SecurityException e) {
+			log.error(SECURITY_EXCEPTION_MSG, method.getName(), param, classz, e);
 		}
 		return o;
 	}
-/**
- * 
- * @param classz
- * @param methodName
- * @param objects
- * @return
- */
-	public static Object callWebServiceMethodWithSystemLogging(Class<? extends Service> classz, String methodName, Object... objects) {
+
+	/**
+	 * Util that allows to call web service of every web service passing
+	 * WebServiceClient class, method name and parameters if needed. Keep
+	 * attention to specify right parameters to prevent every failure during
+	 * execution. This method works with system logging of SOAP request and
+	 * response enabled
+	 * 
+	 * @param classz
+	 *            : Generic class object tha extends
+	 *            {@link javax.xml.ws.Service}+
+	 * 
+	 * @param methodName
+	 *            : Name of Web Service method to invoke
+	 * 
+	 * @param objects
+	 *            : Input parameters of Web Service
+	 * 
+	 * @return Response bject of Web Service
+	 */
+	public static Object callWebServiceMethodWithSystemLogging(Class<? extends Service> classz, String methodName,
+			Object... objects) {
 		SoapRequestResponseLogger ws = SoapRequestResponseLogger.getInstance();
-		
+
 		ws.enableAutomaticWSLog();
-		
+
 		return callWebServiceMethod(classz, methodName, objects);
 	}
+
 	/**
 	 * 
 	 * @param classz
@@ -141,33 +192,25 @@ public class SoapWSInitializer {
 		Object service = inizializeWebServiceClient(classz);
 		List<Method> methods = getMethodsAnnotatedWith(classz, WebEndpoint.class, false);
 		Method method = methods.get(0);
-		
-		
+		String param = StringUtils.EMPTY;
 		try {
 			Object port = method.invoke(service);
 
 			o = invokeMethod(methodName, port, objects);
-			
+
 		} catch (IllegalAccessException e) {
-			log.error("Could not call method {0} of specified class {1} with specified param {2} cause {3}", methodName,
-					classz, objects, e);
+			log.error(ILLEGAL_ACCESS_EXCEPTION_MSG, method.getName(), param, classz, e);
 		} catch (IllegalArgumentException e) {
-			log.error("Could not call method {0} of specified class {1} with specified param {2} cause {3}", methodName,
-					classz, objects, e);
+			log.error(ILLEGAL_ARGUMENT_EXCEPTION_MSG, method.getName(), param, classz, e);
 		} catch (InvocationTargetException e) {
-			log.error("Could not call method {0} of specified class {1} with specified param {2} cause {3}", methodName,
-					classz, objects, e);
-		} catch (NoSuchMethodException e) {
-			log.error("Could not call method {0} of specified class {1} with specified param {2} cause {3}", methodName,
-					classz, objects, e);
+			log.error(INVOCATION_TARGET_EXCEPTION_MSG, method.getName(), param, classz, e);
 		} catch (SecurityException e) {
-			log.error("Could not call method {0} of specified class {1} with specified param {2} cause {3}", methodName,
-					classz, objects, e);
+			log.error(SECURITY_EXCEPTION_MSG, method.getName(), param, classz, e);
 		}
 
 		return o;
 	}
-	
+
 	/**
 	 * 
 	 * @param classz
@@ -175,59 +218,91 @@ public class SoapWSInitializer {
 	 * @param objects
 	 * @return
 	 */
-	public static Object callWebServiceMethodWithCustomLogging(Class<? extends Service> classz, String methodName, Object... objects) {
+	public static Object callWebServiceMethodWithCustomLogging(Class<? extends Service> classz, String methodName,
+			Object... objects) {
 
 		Object o = null;
 		Object service = inizializeWebServiceClient(classz);
 		List<Method> methods = getMethodsAnnotatedWith(classz, WebEndpoint.class, false);
 		Method method = methods.get(0);
-		
+		String param = StringUtils.EMPTY;
+
 		try {
 			Object port = method.invoke(service);
-			
+
 			SoapRequestResponseLogger ws = SoapRequestResponseLogger.getInstance();
 
 			ws.enableAutomaticWSLog();
-			
+
 			o = invokeMethod(methodName, port, objects);
-			
+
 		} catch (IllegalAccessException e) {
-			log.error("Could not call method {0} of specified class {1} with specified param {2} cause {3}", methodName,
-					classz, objects, e);
+			log.error(ILLEGAL_ACCESS_EXCEPTION_MSG, method.getName(), param, classz, e);
 		} catch (IllegalArgumentException e) {
-			log.error("Could not call method {0} of specified class {1} with specified param {2} cause {3}", methodName,
-					classz, objects, e);
+			log.error(ILLEGAL_ARGUMENT_EXCEPTION_MSG, method.getName(), param, classz, e);
 		} catch (InvocationTargetException e) {
-			log.error("Could not call method {0} of specified class {1} with specified param {2} cause {3}", methodName,
-					classz, objects, e);
-		} catch (NoSuchMethodException e) {
-			log.error("Could not call method {0} of specified class {1} with specified param {2} cause {3}", methodName,
-					classz, objects, e);
+			log.error(INVOCATION_TARGET_EXCEPTION_MSG, method.getName(), param, classz, e);
 		} catch (SecurityException e) {
-			log.error("Could not call method {0} of specified class {1} with specified param {2} cause {3}", methodName,
-					classz, objects, e);
+			log.error(SECURITY_EXCEPTION_MSG, method.getName(), param, classz, e);
 		}
 
 		return o;
 	}
 
-	private static Object invokeMethod(String methodName, Object instance, Object ... objects)
-			throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-		Object o;
-		Method m;
+	private static Object invokeMethod(String methodName, Object instance, Object... objects) {
 
-		if (objects.length > 0) {
+		Object o = null;
+		Class<? extends Object> classz = instance.getClass();
+		Class<?>[] a = getParametersClass(objects);
+		String param = getString(a);
+		try {
 
-			Class<?>[] a = getParametersClass(objects);
-			m = instance.getClass().getMethod(methodName, a);
+			if (objects.length > 0) {
 
-		} else {
+				Method m = classz.getMethod(methodName, a);
 
-			m = instance.getClass().getMethod(methodName);
+				o = m.invoke(instance, objects);
+
+			} else {
+
+				Method m = instance.getClass().getMethod(methodName);
+				o = m.invoke(instance);
+			}
+		} catch (IllegalAccessException e) {
+			log.error(ILLEGAL_ACCESS_EXCEPTION_MSG, methodName, param, classz, e);
+		} catch (IllegalArgumentException e) {
+			log.error(ILLEGAL_ARGUMENT_EXCEPTION_MSG, methodName, param, classz, e);
+		} catch (InvocationTargetException e) {
+			log.error(INVOCATION_TARGET_EXCEPTION_MSG, methodName, param, classz, e);
+		} catch (NoSuchMethodException e) {
+			log.error(NO_SUCH_METHOD_EXCEPTION_MSG, methodName, param, classz, e);
+		} catch (SecurityException e) {
+			log.error(SECURITY_EXCEPTION_MSG, methodName, param, classz, e);
 		}
-		o = m.invoke(instance, objects);
-		
+
 		return o;
+	}
+
+	/**
+	 * 
+	 * @param a
+	 * @return
+	 */
+	private static String getString(Class<?>[] a) {
+		StringBuilder builder = new StringBuilder();
+
+		List<Class<?>> l = Arrays.asList(a);
+		Iterator<Class<?>> i = l.iterator();
+
+		while (i.hasNext()) {
+			Class<?> classz = i.next();
+			builder.append(classz.getName());
+			if (i.hasNext()) {
+				builder.append(",");
+			}
+
+		}
+		return builder.toString();
 	}
 
 	/**
